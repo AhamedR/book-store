db7a7243869b9d9a565277817ae95c6e46f52670
Ahamed <ahamedrasheedhot@gmail.com>
2024-05-21 10:58:25 +0530

    feat: configure mantine UI

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // ...other config
+  sassOptions: {
+    prependData: `@import "./_mantine.scss";`,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+      {
+        protocol: "https",
+        hostname: "fakeimg.pl",
+      },
+      {
+        protocol: "https",
+        hostname: "random.imagecdn.app",
+      },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
