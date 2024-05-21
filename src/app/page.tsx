b80215623c2aff51f2599a5c202f1a8d1e7e7877
Ahamed <ahamedrasheedhot@gmail.com>
2024-05-21 12:02:18 +0530

    fix: Add no items alert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 "use client";
 
-import { Container, Flex, Pagination } from "@mantine/core";
+import { Alert, Container, Flex, Pagination } from "@mantine/core";
 import { useEffect } from "react";
 
 import BookCard from "@/components/BookCard";
 import Filter from "@/components/Filter";
 import { useBookStore } from "@/providers/bookStoreProvider";
 import CardSkeleton from "@/components/CardSkeleton";
+import { IconInfoCircle } from "@tabler/icons-react";
 
 export default function HeaderMenu() {
-  const { books, currentPage, totalPages, isFetching, fetchBooks } =
-    useBookStore((state) => state);
+  const { books, currentPage, totalPages, isFetching, fetchBooks } = useBookStore(
+    (state) => state
+  );
 
   useEffect(() => {
     fetchBooks();
@@ -41,6 +43,19 @@ export default function HeaderMenu() {
               description={book.description}
             />
           ))}
+          {!isFetching && books.length === 0 ? (
+            <Alert
+              variant="outline"
+              color="yellow"
+              radius="md"
+              title="Sorry!"
+              icon={<IconInfoCircle />}
+              mt="xl"
+              w="100%"
+            >
+              We could not find any books for your preference.
+            </Alert>
+          ) : null}
         </Flex>
       </Container>
       <Flex mih={80} mt={20} gap="md" justify="center" align="center">
